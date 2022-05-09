11d3458d2ed4e69f99b76c241fe11d175c422eee
hanhaichao <93064796+hanhaichao@users.noreply.github.com>
2022-05-09 09:37:32 +0800

    kafka改版
    把rabbiMq换成kafka

--- tensquare/tensquare_sms/src/main/java/com/tensquare/sms/listener/SmsListener.java
+++ tensquare/tensquare_sms/src/main/java/com/tensquare/sms/listener/SmsListener.java
@@ -2,16 +2,17 @@ package com.tensquare.sms.listener;
 
 import com.aliyuncs.exceptions.ClientException;
 import com.tensquare.sms.utils.SmsUtil;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import lombok.extern.slf4j.Slf4j;
 
 import java.util.Map;
 
 @Component
-@RabbitListener(queues = "sms")
+@Slf4j
+@KafkaListener(topics = {"sms"})
 public class SmsListener {
     @Autowired
     private SmsUtil smsUtil;
@@ -20,12 +21,14 @@ public class SmsListener {
     @Value("${aliyun.sms.sign_name}")
     private String sign_name;
 
-    @RabbitHandler
-    public void executeSms(Map<String, String> map) {
+    @KafkaListener(topics = {"test"})
+    public void consumerSms(Map<String, String> map){
         String mobile = map.get("mobile");
         String checkcode = map.get("checkcode");
         System.out.println("手机号" + map.get("mobile"));
+        log.info("手机号" + map.get("mobile"));
         System.out.println("验证码" + map.get("checkcode"));
+        log.info("验证码" + map.get("checkcode"));
         try {
             smsUtil.sendSms(mobile, template_code, sign_name, "{\"checkcode\":\"" + checkcode + "\"}");
         } catch (ClientException e) {
